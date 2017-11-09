59231a5911cc784798a72cf2ef113340a5a0b24f
Michael R Fleet <f1337@f1337.us>
2017-11-08 23:22:35 -0500

    remove CGO_ENABLED=1 from gob.fish

--- .config/fish/functions/gob.fish
+++ .config/fish/functions/gob.fish
@@ -1,3 +1,3 @@
 function gob
-  docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app -e GOOS=darwin -e GOARCH=amd64 -e CGO_ENABLED=1 golang:1.9 go build $argv
+  docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app -e GOOS=darwin -e GOARCH=amd64 golang:1.9 go build $argv
 end
