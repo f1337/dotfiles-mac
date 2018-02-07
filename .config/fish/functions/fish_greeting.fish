d12ceb0337f80c02f0f1ef523e495f9e7e8064e8
Michael R Fleet <f1337@f1337.us>
2018-02-07 11:26:26 -0500

    use fish for cowsay again

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -2,5 +2,5 @@ function fish_greeting
   set_color $fish_color_autosuggestion
   uptime
   set_color normal
-  echo "There is no box." | pandathink | lolcat
+  echo "There is no box." | fishthink | lolcat
 end
