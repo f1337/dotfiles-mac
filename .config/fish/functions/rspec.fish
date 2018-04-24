26b94a42e26b6b18f878d1b0902656b32bd34138
Michael R Fleet <f1337@f1337.us>
2018-04-24 00:34:48 -0400

    add RSPEC_RETRY_RETRY_COUNT=0 to rspec.fish

--- .config/fish/functions/rspec.fish
+++ .config/fish/functions/rspec.fish
@@ -1,3 +1,3 @@
 function rspec
-	docker-compose run --rm -e SKIP_COVERAGE=true test bundle exec rspec $argv
+	docker-compose run --rm -e RSPEC_RETRY_RETRY_COUNT=0 -e SKIP_COVERAGE=true test bundle exec rspec $argv
 end
